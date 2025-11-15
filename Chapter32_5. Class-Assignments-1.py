be825463f7054fc83b582bbdac8d5ffe52571262
mrk79blr <mrk79blr@gmail.com>
2025-11-14 20:27:24 -0500

    Update Chapter32_5. Class-Assignments-1.py

--- Chapter32_5. Class-Assignments-1.py
+++ Chapter32_5. Class-Assignments-1.py
@@ -1,2 +1,20 @@
-import SubfieldsInAI from SubfieldsInAI
+from SubfieldsInAI import SubfieldsInAI
+from OddEven import OddEven
+from EligibilityForMarriage import EligibilityForMarriage
+from FindPercent import FindPercent
+from triangle import triangle
+
+print("Printing the List") 
 SubfieldsInAI.Subfields()
+
+print ("Find Odd/Even")
+OddEven.OddEven()
+
+print ("Enter male/female and the age to know the eligibility")
+EligibilityForMarriage.Eligible()
+
+print("To calculate the percentage of 5 subjects")
+FindPercent.percentage()
+
+print ("To calculate area and perimeter of a triangle")
+triangle.triangle()
